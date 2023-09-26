71611276ecd1e55bd019bab379209b89ba1d5363
kenil-kanani <kenilkananigecbvn@gmail.com>
2023-09-26 12:38:44 +0530

    Added service for uplode file

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -78,6 +78,54 @@ export class Service {
             return false
         }
     }
+
+    async getPosts(queries = [Query.equal("status", "active")]) {
+        try {
+            return await this.databases.listDocuments(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                queries
+            )
+        } catch (error) {
+            console.log("Appwrite Service :: Get Posts Error :: ", error);
+            return false
+        }
+    }
+
+    //! File uplode service
+
+    async uploadFile(file) {
+        try {
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique,
+                file
+            )
+        } catch (error) {
+            console.log("Appwrite Service :: Upload File Error :: ", error);
+            return false
+        }
+    }
+
+    async deleteFile(fileId) {
+        try {
+            await this.bucket.deleteFile(
+                conf.appwriteBucketId,
+                fileId
+            )
+            return true
+        } catch (error) {
+            console.log("Appwrite Service :: Delete File Error :: ", error);
+            return false
+        }
+    }
+
+    getFilePreview(fileId) {
+        return this.bucket.getFilePreview(
+            conf.appwriteBucketId,
+            fileId
+        )
+    }
 }
 
 const service = new Service()
