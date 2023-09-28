7b214f8a094ef7d4dc7ca860c1ff7d563fc86eeb
kenil-kanani <kenilkananigecbvn@gmail.com>
2023-09-28 14:42:19 +0530

    Made some components like Footer, Header , Input , Btn etc...

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,47 @@
 import './App.css'
+import { useState, useEffect } from 'react'
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+import { useDispatch } from 'react-redux'
+import { Header, Footer } from './components'
+import { Outlet } from 'react-router-dom'
+
 
 function App() {
 
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        }
+        else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
   console.log()
 
-  return (
-    <h1>Hello Sariyane...</h1>
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
   )
+    :
+    (
+      null
+    )
 }
 
 export default App
